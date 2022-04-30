5874266c801186b886599e69f356c0576b86ac4b
pdarwin <46503478+pdarwin@users.noreply.github.com>
2022-04-30 20:12:52 +0100

    act

--- src/js/model.js
+++ src/js/model.js
@@ -1,25 +1,25 @@
+import { API_URL } from "./config";
+import { getJson } from "./helpers";
+
 export const state = { recipe: {} };
 
 export const loadRecipe = async id => {
-  const res = await fetch(
-    `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
-  );
-  const data = await res.json();
+  try {
+    const data = await getJson(`${API_URL}/${id}`);
 
-  if (!res.ok) throw new Error(`${data.message} (${res.status})`);
-
-  console.log(res, data);
-
-  let { recipe } = data.data;
-  state.recipe = {
-    id: recipe.id,
-    title: recipe.title,
-    publisher: recipe.publisher,
-    sourceUrl: recipe.source_url,
-    image: recipe.image_url,
-    servings: recipe.servings,
-    cookingTime: recipe.cooking_time,
-    ingredients: recipe.ingredients,
-  };
-  console.log(state.recipe);
+    let { recipe } = data.data;
+    state.recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.source_url,
+      image: recipe.image_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients,
+    };
+    console.log(state.recipe);
+  } catch (err) {
+    console.error(err, "😘");
+  }
 };
